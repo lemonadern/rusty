132a81b74866318ec9446bf4599e70c8e4000c04
Taishi Naka <taishi.develop@gmail.com>
2025-02-15 11:41:15 +0900

    add drop (TRPL 15.3)

---
+++ examples/drop/src/main.rs
@@ -0,0 +1,35 @@
+///
+/// The Rust Programming Language 15.3 Drop
+/// https://doc.rust-jp.rs/book-ja/ch15-03-drop.html
+/// 
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!(
+            "Dropping `CustomSmartPointer` with data `{}` !!!",
+            self.data
+        );
+    }
+}
+
+#[allow(unused)]
+fn main() {
+    let a = CustomSmartPointer {
+        data: String::from("My stuff"),
+    };
+
+    let b = CustomSmartPointer {
+        data: String::from("Other stuff"),
+    };
+
+    println!("CustomSmartpointers created.");
+}
+
+// Output:
+// CustomSmartpointers created.
+// Dropping `CustomSmartPointer` with data `Other stuff` !!!
+// Dropping `CustomSmartPointer` with data `My stuff` !!!
